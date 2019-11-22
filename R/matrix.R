0ddd11db7222bacfe7726937c480c9f1f287f7ed
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2019-11-22 12:19:39 +0100

    Update matrix.R

--- R/matrix.R
+++ R/matrix.R
@@ -36,7 +36,7 @@ m <- function(...){
   # Cases for unit tests
   # working: m(1, 2, NA | 4, NA, 6 | (1 | 2), 8, 9)
   # not working: m(1, 2, NA | 4, NA, (6 | 9) | 7, 8, 9)
-  # m( 1:3 | 2:4 ), cbind(1:10) non-intuitive behavior
+  # m( 1:3 | 2:4 ), cbind(1:10) rbind(1:10) non-intuitive(?) behavior
 }
 
 # m(1, 2, 3 | 4, 5, 6 | 7, 8, (9 + 1))
